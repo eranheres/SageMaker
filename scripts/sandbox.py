5a4bbb948e668491e0f1a217762b766f1879ccc3
EC2 Default User <ec2-user@ip-10-192-10-47.ec2.internal>
2018-12-27 15:17:50 +0000

    first commit

---
+++ scripts/sandbox.py
@@ -0,0 +1,13 @@
+from object_detection_utils import convert_augmented_samples_to_files_samples
+
+## convert_augmented_samples_to_files_samples(
+##    'XButton-train',
+##    'com.tabtale.dev.eran', 
+##    'projects/object_detection/input/xbutton/train/labels/XButton-train/manifests/output/output.manifest',
+##    '/test_output/train') 
+    
+convert_augmented_samples_to_files_samples(
+    'XButton-validation',
+    'com.tabtale.dev.eran', 
+    'projects/object_detection/input/xbutton/validation/labels/XButton-validation/manifests/output/output.manifest',
+    '/test_output/validation') 
